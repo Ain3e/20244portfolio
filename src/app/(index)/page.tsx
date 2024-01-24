08b316382019cdba3f1a6f450a173c16357b00bd
Alex Luowan <aaluowan@gmail.com>
2024-01-24 14:41:08 -0800

    desktop layout fixed - homepage

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -79,6 +79,8 @@ export default function Home() {
                     >
                     </Image>
                 </a>
+                <hr className="col-start-1 col-span-12 mt-1 border-black">
+                </hr>
                 <div className="col-start-1 row-start-2 font-favorit text-xl col-span-3 lg:col-span-4 ">
                     <h4 className="mt-1">88rising</h4>
                     {/*<h4 className="-mt-1 text-customGray ">Experience Design</h4>*/}
@@ -112,7 +114,7 @@ export default function Home() {
                     <h4 className="mt-thumbnail-top text-right">2022 - INTERACTION DESIGN, ART DIRECTION</h4>
                 </div>
 
-                <a className="col-start-1 col-span-full -mt-36 lg:mt-0 lg:col-start-13 lg:col-span-12 "
+                <a className="col-start-1 col-span-full -mt-36 lg:mt-0 lg:col-start-13 lg:col-span-12 row-start-1"
                    href="/projects/givemeyesterday">
 
                     <Image src="/img/givemeyesterday/givemeyesterday-thumbnail.png"
@@ -123,6 +125,66 @@ export default function Home() {
                     >
                     </Image>
                 </a>
+                <hr className="col-start-13 col-span-12 mt-1 border-black">
+                </hr>
+            </div>
+            <div className="mx-8 grid grid-cols-6 md:grid-cols-12 lg:grid-cols-24 gap-x-5 mt-7">
+                <a className="col-start-1 col-span-full lg:col-span-12" href="/projects/88rising">
+                    <Image src="/img/88rising/88rising-thumbnail.png"
+                           alt="a laptop"
+                           width={3075}
+                           height={1672}
+                    >
+                    </Image>
+                </a>
+                <hr className="col-start-1 col-span-12 mt-1 border-black">
+                </hr>
+                <div className="col-start-1 row-start-2 font-favorit text-xl col-span-3 lg:col-span-5 ">
+                    <h4 className="mt-1">Dutch Design Awards</h4>
+                    {/*<h4 className="-mt-1 text-customGray ">Experience Design</h4>*/}
+                </div>
+                <div
+                    className="lg:col-start-6 row-start-2 font-favorit text-bodycopy lg:col-span-6 invisible lg:visible">
+                    {/*<h4 className="text-customGray mt-thumbnail-top leading-bodyPara">A transformed digital*/}
+                    {/*    interface to*/}
+                    {/*    help fans*/}
+                    {/*    to explore and uplift emerging artists</h4>*/}
+                </div>
+                <div
+                    className="col-start-6 md:col-start-12 lg:col-start-7 row-start-2 font-favoritmono text-base lg:col-span-6">
+                    <h4 className="mt-thumbnail-top text-right">2023 - INTERACTION DESIGN/ART DIRECTION</h4>
+                </div>
+
+                <div
+                    className="lg:col-start-13 col-start-1 col-span-4 row-start-5 lg:row-start-2 font-favorit text-xl lg:col-span-6">
+                    <h4 className="mt-1">Brian Jessel BMW</h4>
+                    <h4 className="-mt-1 text-customGray"></h4>
+                </div>
+
+                {/*<div*/}
+                {/*    className="lg:col-start-18 row-start-2 font-favorit text-bodycopy lg:col-span-6 invisible lg:visible">*/}
+                {/*    <h4 className="text-customGray mt-thumbnail-top leading-bodyPara">A compelling online preview for an*/}
+                {/*        upcoming*/}
+                {/*        exhibition, enticing visitors to experience the works in person.</h4>*/}
+                {/*</div>*/}
+                <div
+                    className="lg:col-start-18 md:col-start-12 col-start-6 row-start-5 lg:row-start-2 font-favoritmono text-base col-span-7">
+                    <h4 className="mt-thumbnail-top text-right">2023 - INTERACTION DESIGN, UX RESEARCH</h4>
+                </div>
+
+                <a className="col-start-1 col-span-full -mt-36 lg:mt-0 lg:col-start-13 lg:col-span-12 row-start-1"
+                   href="/projects/givemeyesterday">
+
+                    <Image src="/img/givemeyesterday/givemeyesterday-thumbnail.png"
+                           alt="a laptop"
+                           width={3075}
+                           height={1672}
+                           className=""
+                    >
+                    </Image>
+                </a>
+                <hr className="col-start-13 col-span-12 mt-1 border-black">
+                </hr>
             </div>
 
         </main>
