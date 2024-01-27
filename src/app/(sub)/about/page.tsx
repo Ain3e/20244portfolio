f19a4a3f6a01698bf37cc3369b98f36f02f30559
Aine <aaluowan@gmail.com>
2024-01-26 20:06:44 -0800

    Finished about page

--- src/app/(sub)/about/page.tsx
+++ src/app/(sub)/about/page.tsx
@@ -5,57 +5,57 @@ export default function Home() {
     return (
         <main className="antialiased gap-y-0">
             <title>About Me</title>
-            <div className="bg-white fixed top-0 left-0 right-0 z-50">
-                <nav className="mx-8 grid grid-cols-6 lg:grid-cols-24 gap-x-5 font-favorit text-xs py-4">
-                    <ul className="flex flex-row col-span-full lg:col-span-12 gap-3">
-                        <li><a href="/" className="">ALEX LUOWAN</a></li>
-                        <li>—</li>
-                        <li><a href="/" className="">
+            <div className="bg-dirtywhitebg fixed top-0 left-0 right-0 z-50">
+                <nav className="mx-8 grid grid-cols-6 lg:grid-cols-24 md:grid-cols-12 gap-x-5 font-favoritmono text-base py-4 -mb-4 md:mb-0 lg:mb-0">
+                    <div className="col-start-1 row-start-1 col-span-2 lg:col-span-4">
+                        <a href="/" className="">ALEX LUOWAN</a>
+                    </div>
+                    <ul className="flex flex-row col-span-3 lg:col-start-22 md:col-start-10 row-start-1 justify-end -mt-1 col-start-4">
+                        <li></li>
+                        <li><a href="/" className="px-3">
                             <button
-                                className="hover:underline ease-in-out duration-300 hover:text-hoverGray">WORK
+                                className="hover:border-orangehighlight hover:outline hover:outline-1 hover:bg-dirtywhitebg ease-in-out duration-300 hover:text-orangehighlight text-black px-3 py-1">WORK
                             </button>
                         </a></li>
-                        <li><a href="/about" className="">
+                        <li><a href="/about" className="px-3">
                             <button
-                                className="hover:underline ease-in-out duration-300 text-customGray underline hover:text-hoverGray">ABOUT
+                                className="hover:border-orangehighlight hover:bg-dirtywhitebg ease-in-out duration-300 hover:text-orangehighlight border text-black bg-orangehighlight px-3 py-1 outline border-black outline-0">ABOUT
                             </button>
                         </a></li>
                         <li><a href="https://drive.google.com/file/d/1cDINhKrYRQcKnyS8bgtY_3AvWAg-Hyun/view?usp=sharing"
-                               className="" target="_blank">
+                               className="pl-3" target="_blank">
                             <button
-                                className="hover:underline ease-in-out duration-300 hover:text-hoverGray">RESUME
+                                className="hover:border-orangehighlight hover:outline hover:outline-1 hover:bg-dirtywhitebg ease-in-out duration-300 hover:text-orangehighlight text-black px-3 py-1">RESUME
                             </button>
                         </a></li>
                     </ul>
-                    {/*<div className="col-start-24">*/}
-                    {/*    <a href="/" className="">*/}
-                    {/*        <button*/}
-                    {/*            className="hover:underline ease-in-out duration-300 hover:text-hoverGray ml-6" >INDEX*/}
-                    {/*        </button>*/}
-                    {/*    </a>*/}
-                    {/*</div>*/}
+                    <div className="lg:col-span-5 md:col-span-5 lg:col-start-9 row-start-1 md:col-start-4 invisible md:visible lg:visible">
+                        <p>INTERACTION/PRODUCT DESIGNER</p>
+                    </div>
                 </nav>
             </div>
 
             <div className="mx-8 grid grid-cols-6 md:grid-cols-12 lg:grid-cols-24 gap-x-5 mt-40 lg:mt-home-top">
                 <div className="lg:col-span-14 md:col-span-9 col-span-full">
-                    <h4 className="font-favorit font-thin lg:text-base">
+                    <h4 className="font-favoritmono font-thin lg:text-base text-orangehighlight">
                         WHO AM I
                     </h4>
-                    <h2 className="font-eiko font-thin text-xl lg:text-2xl mt-5 text-customGray leading-aboutpage md:leading-aboutpage lg:leading-aboutpage">
+                    <h2 className="font-montreal font-thin text-xl lg:text-2xl mt-5 text-black leading-aboutpage md:leading-aboutpage lg:leading-aboutpage">
                         I am a <span className="text-black">design student located in Vancouver, Canada,</span> currently studying at Simon Fraser
                         University.
-                        When I&#39;m not engrossed in front of a screen, you can find me playing rhythm games or taking
+                        In my free time, you can find me playing rhythm games or taking
                         photography. I firmly believe that <span className="text-black"> impactful design is rooted in a harmonious blend of
                         functionality, storytelling, and a commitment to social good. </span>
                     </h2>
                 </div>
-                <a href="mailto:aluowan@sfu.ca" className="row-start-2 mt-10 col-span-5 col-start-1">
+                <div  className="row-start-2 mt-10 col-span-5 col-start-1">
+                <a href="mailto:aluowan@sfu.ca">
                     <button
-                        className="font-favorit text-base border-black py-3 px-16 outline outline-1 hover:bg-black hover:border-white hover:text-white ease-in-out duration-300 ">
+                        className="font-favorit text-base border-black py-3 px-16 outline outline-1 hover:bg-orangehighlight hover:border-white hover:text-black ease-in-out duration-300 ">
                         Contact Me
                     </button>
                 </a>
+                </div>
 
             </div>
 
